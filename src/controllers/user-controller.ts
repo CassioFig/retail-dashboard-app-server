bebcb8b73e85296340fdbbf6182aca8a9cdf3091
Cassio Figueiredo <cassio.figueiredof@gmail.com>
2025-06-23 17:32:27 -0700

    feat: Add User Controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,42 @@
+import { database, DatabaseCollections } from "../database";
+import { Request, Response } from 'express';
+import { User } from "../models";
+import cuid from 'cuid';
+
+class UserController {
+	async signUp(req: Request, res: Response) {
+		try {
+			const body = req.body as User;
+			const existingUser = await database.findBy<User>(DatabaseCollections.USERS, user => user.email === body.email);
+			if (existingUser.length > 0) {
+				return res.status(400).json({ message: 'User with this email already exists' });
+			}
+
+			const user = await database.create(DatabaseCollections.USERS, { ...body, id: cuid() });
+			const { password, ...userWithouPassword } = user;
+			res.status(201).json({ message: 'User created successfully', userWithouPassword });
+		} catch (error) {
+			console.error('Error signing up user:', error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	}
+
+	async login(req: Request, res: Response) {
+		try {
+			const { email, password } = req.body as { email: string; password: string };
+			const user = await database.findBy<User>(DatabaseCollections.USERS, user => user.email === email && user.password === password);
+
+			if (user.length === 0) {
+				return res.status(401).json({ message: 'Invalid email or password' });
+			}
+
+			const { password: _, ...userWithoutPassword } = user[0];
+			res.status(200).json({ message: 'Login successful', userWithoutPassword });
+		} catch (error) {
+			console.error('Error logging in user:', error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	}
+}
+
+export const userController = new UserController();
